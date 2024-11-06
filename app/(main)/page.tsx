777b135364a3dee11c24c25ffb521a22cdce0942
priyankasoni930 <priyankasoni93016@gmail.com>
2024-11-06 19:25:37 +0530

    ...

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -116,11 +116,11 @@ export default function Home() {
   }, [loading, generatedCode]);
 
   return (
-    <div className="mx-auto flex min-h-screen max-w-7xl flex-col items-center justify-center bg-black py-2">
+    <div className="mx-auto flex min-h-screen max-w-full flex-col items-center justify-center bg-black py-2">
       <main className="mt-12 flex w-full flex-1 flex-col items-center px-4 text-center sm:mt-20">
         <a
           className="mb-4 inline-flex h-7 shrink-0 items-center gap-[9px] rounded-[50px] border-[0.5px] border-solid border-[#E6E6E6] bg-[rgba(234,238,255,0.65)] bg-gray-100 px-7 py-5 shadow-[0px_1px_1px_0px_rgba(0,0,0,0.25)]"
-          href="https://ai.google.dev/tutorials/web_quickstart"
+          href="https://ai.google.dev/"
           target="_blank"
         >
           <span className="text-center">
